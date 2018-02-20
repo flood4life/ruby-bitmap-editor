da59df11ee503bc6d366ccd9a948ed4e4fd2c97b
Murad Yusufov <flood4life@gmail.com>
2018-02-20 16:19:59 +0300

    Fix specs for BitmapCommandExecutor

--- spec/bitmap_command_executor_spec.rb
+++ spec/bitmap_command_executor_spec.rb
@@ -30,16 +30,22 @@ O O
       map
       expect {
         subject.call(method: :to_s)
-      }.to output(expected)
+      }.to output(expected).to_stdout
     end
 
     it 'forwards :set, :clear, :draw_vertical_line and :draw_horizontal_line to the editor' do
       subject.call(method: :new, arguments: { width: 2, height: 2 })
       editor = subject.editor
+      allow(editor).to receive(:clear).and_call_original
+      allow(editor).to receive(:set).and_call_original
+      allow(editor).to receive(:draw_vertical_line).and_call_original
+      allow(editor).to receive(:draw_horizontal_line).and_call_original
+
       subject.call(method: :clear)
       subject.call(method: :set, arguments: { x: 1, y: 1, color: 'A' })
       subject.call(method: :draw_vertical_line, arguments: { x: 1, y1: 1, y2: 1, color: 'A' })
       subject.call(method: :draw_horizontal_line, arguments: { x1: 1, x2: 1, y: 1, color: 'A' })
+
       expect(editor).to have_received(:clear)
       expect(editor).to have_received(:set).with(x: 1, y: 1, color: 'A')
       expect(editor).to have_received(:draw_vertical_line).with(x: 1, y1: 1, y2: 1, color: 'A')
