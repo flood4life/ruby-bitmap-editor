298dc392ba094e88a5bc3e4b5ac873585614231d
Murad Yusufov <flood4life@gmail.com>
2018-02-20 10:45:28 +0300

    Add argument type check to BitmapEditor#initialize

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,5 +1,15 @@
 class BitmapEditor
   def initialize(width:, height:)
+    check_argument_type('height', height)
+    check_argument_type('width', width)
 
+  end
+
+  private
+
+  def check_argument_type(name, value)
+    unless value.is_a?(Integer)
+      raise ArgumentError, "#{name.capitalize} (#{value}:#{value.class}) is not an Integer"
+    end
   end
 end
