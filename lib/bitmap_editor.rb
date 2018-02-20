287068c4d75102cf2b35bb8c91a0cb3805f140f0
Murad Yusufov <flood4life@gmail.com>
2018-02-20 10:55:43 +0300

    Add argument value check to BitmapEditor#initialize

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,7 +1,11 @@
 class BitmapEditor
+  VALUE_RANGE = (1..250)
+
   def initialize(width:, height:)
     check_argument_type('height', height)
     check_argument_type('width', width)
+    check_argument_value('height', height, VALUE_RANGE)
+    check_argument_value('width', width, VALUE_RANGE)
 
   end
 
@@ -11,5 +15,17 @@ class BitmapEditor
     unless value.is_a?(Integer)
       raise ArgumentError, "#{name.capitalize} (#{value}:#{value.class}) is not an Integer"
     end
+  end
+
+  def check_argument_value(name, value, range)
+    if value < range.first
+      raise ArgumentError, value_error_message(name, value, range.first, 'lower')
+    elsif value > range.last
+      raise ArgumentError, value_error_message(name, value, range.last, 'bigger')
+    end
+  end
+
+  def value_error_message(name, value, boundary, relation)
+    "#{name.capitalize} (#{value}) is #{relation} than #{boundary}"
   end
 end
---
+++ lib/bitmap_transformer.rb
@@ -0,0 +1,5 @@
+class BitmapTransformer
+  class << self
+
+  end
+end
