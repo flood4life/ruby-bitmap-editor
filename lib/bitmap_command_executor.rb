dede3003db42c490d59f3b1ce650a624989e69af
Murad Yusufov <flood4life@gmail.com>
2018-02-20 16:20:35 +0300

    Add implementation for BitmapCommandExecutor

---
+++ lib/bitmap_command_executor.rb
@@ -0,0 +1,23 @@
+require 'bitmap_editor'
+
+class BitmapCommandExecutor
+  attr_reader :editor
+
+  def call(method:, arguments: nil)
+    if method != :new && @editor.nil?
+      raise StandardError, "Bitmap is not initialized"
+    end
+    case method
+    when :new
+      @editor = BitmapEditor.new(arguments)
+    when :to_s
+      puts editor.to_s
+    when :clear
+      editor.clear
+    when :set, :draw_vertical_line, :draw_horizontal_line
+      editor.send(method, **arguments)
+    else
+      raise ArgumentError, "Unknown method :#{method}"
+    end
+  end
+end
